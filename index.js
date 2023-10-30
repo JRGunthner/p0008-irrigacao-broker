b99fdeb0aa65b6b281b443095e999d86d9cf18e7
JRGunthner <juliano.gunthner@gmail.com>
2023-10-30 07:09:50 -0300

    traduções

--- index.js
+++ index.js
@@ -1,37 +1,35 @@
-const mqtt = require('mqtt')
+const mqtt = require("mqtt");
+const express = require("express");
+const app = express();
+const PORT_SERVER = 80;
+const HOST_SERVER = "0.0.0.0";
 
-const express = require('express')
-const app = express()
-const PORT_SERVER = 80
-const HOST_SERVER = "0.0.0.0"
+app.use(express.static("templates"));
 
-app.use(express.static('templates'));
-
-const host = 'irrigacao.jgtche.com.br'
-const port = '1883'
-const clientId = `node_mqtt_${Math.random().toString(16).slice(3)}`
-
-const connectUrl = `mqtt://${host}:${port}`
+const host = "irrigacao.jgtche.com.br";
+const port = "1883";
+const clientId = `node_mqtt_${Math.random().toString(16).slice(3)}`;
+const connectUrl = `mqtt://${host}:${port}`;
 
 const client = mqtt.connect(connectUrl, {
     clientId,
     clean: true,
     connectTimeout: 4000,
-    username: 'mastche',
-    password: '123456789',
-    reconnectPeriod: 1000,
-})
+    username: "mastche",
+    password: "123456789",
+    reconnectPeriod: 1000
+});
 
-const topic_con = '/nodejs/mqtt'
-const topic_ligar = 'servidor/resposta'
-const topic_desligar = 'servidor/resposta'
+const topic_con = "nodejs/mqtt";
+const topic_ligar ="servidor/resposta";
+const topic_desligar = "servidor/resposta";
 
-client.on('connect', () => {
-    console.log('Connected')
+client.on("connect", () => {
+    console.log("Conectado ao servidor MQTT");
 
     client.subscribe([topic_con], () => {
-        console.log(`Subscribe to topic '${topic_con}'`)
-        client.publish(topic_con, 'Broker conectado', { qos: 0, retain: false }, (error) => {
+        console.log(`Enviando '${topic_con}'`)
+        client.publish(topic_con, "Broker conectado", { qos: 0, retain: false }, (error) => {
             if (error) {
                 console.error(error)
             }
@@ -39,11 +37,11 @@ client.on('connect', () => {
     })
 })
 
-client.on('message', (topic, payload) => {
-    console.log('Received Message:', topic, payload.toString())
-})
+client.on("message", (topic, payload) => {
+    console.log("Mensagem recebida:", topic + ":", payload.toString())
+});
 
-app.post('/ligar_motor', (req, res) => {
+app.post("/ligar_motor", (req, res) => {
     const click = { clickTime: new Date() };
     console.log(click);
 
@@ -53,7 +51,7 @@ app.post('/ligar_motor', (req, res) => {
     }
 
     client.subscribe([topic_ligar], () => {
-        console.log(`Subscribe to topic '${topic_ligar}'`)
+        console.log(`Enviando: '${topic_ligar}'`)
         client.publish(topic_ligar, JSON.stringify(msg), { qos: 0, retain: false }, (error) => {
             if (error) {
                 console.error(error)
@@ -62,11 +60,10 @@ app.post('/ligar_motor', (req, res) => {
     })
 });
 
-
-app.post('/desligar_motor', (req, res) => {
+app.post("/desligar_motor", (req, res) => {
     const click = { clickTime: new Date() };
     console.log(click);
-    
+
     let msg = {
         "Id": 1,
         "Msg": "desligar"
@@ -82,9 +79,9 @@ app.post('/desligar_motor', (req, res) => {
     })
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-})
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 app.listen(PORT_SERVER, HOST_SERVER, () => {
     console.log(`Server running at http://${HOST_SERVER}:${PORT_SERVER}/`);
